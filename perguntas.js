ebd4a871a05b80dfd92a4c4c747fb9dc148a16b3
vickvaporu6 <00001064285077sp@al.educacao.sp.gov.br>
2024-11-21 11:43:45 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Geografia',
+    'Qual é a capital do Brasil',
+    ' Brasília'
+)
+
+criaCartao(
+    'Arte',
+    'Quem pintou a Mona Lisa?',
+    ' Leonardo da Vinci.'
+)
+
+criaCartao(
+    'Astrologia',
+    'Qual é o maior planeta do Sistema Solar?',
+    'Júpiter'
+)
+
+criaCartao(
+    'Geografia',
+    'Em que continente fica o Egito?',
+    'África'
+)
